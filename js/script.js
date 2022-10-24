7383c77e48081220df4aafe74d6511e03c5d93af
Konstantin Uglov <real.konstantin.uglov@gmail.com>
2022-10-24 19:17:41 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?');
+
+let personalMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastFilm = prompt('Один из последних просмотренных фильмов ?');
+let lastFilmScore = +prompt('На сколько оцените его ?');
+
+// personalMovieDB.movies = {
+//     lastFilm: lastFilmScore
+// };
+
+personalMovieDB['movies'] = {
+    lastFilm: lastFilmScore
+};
+
+console.log(personalMovieDB);
